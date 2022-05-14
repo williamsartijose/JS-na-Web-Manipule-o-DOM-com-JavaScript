6860d8acaadadb0910a4a5e7cfaa4173cfde069f
williamsartijose <williamsartijose@hotmail.com>
2022-05-14 15:01:21 -0300

    Conhecendo o DOM

---
+++ assets/css/listaDeTarefas.js
@@ -0,0 +1,8 @@
+const novaTarefa = document.querySelector('[data-from-button]')
+
+
+novaTarefa.addEventListener('click', (console.log('fui clicado')))
+
+click 
+novaTarefa
+console.log('fui clicado')
