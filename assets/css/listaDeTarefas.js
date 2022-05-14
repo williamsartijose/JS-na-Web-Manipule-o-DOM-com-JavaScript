369b305c608c427b31e66d227e2370b09e21fe07
williamsartijose <williamsartijose@hotmail.com>
2022-05-14 15:20:28 -0300

    Comportamento do formulario

--- assets/css/listaDeTarefas.js
+++ assets/css/listaDeTarefas.js
@@ -1,8 +1,13 @@
-const novaTarefa = document.querySelector('[data-from-button]')
+const criarTarefa = (evento) => { 
 
+    evento.preventDefault()
 
-novaTarefa.addEventListener('click', (console.log('fui clicado')))
+    const input = document.querySelector('[data-form-input]')
+    const valor = input.value
+    console.log( valor  )
+    input.value = " "
+}
 
-click 
-novaTarefa
-console.log('fui clicado')
+const novaTarefa = document.querySelector('[data-form-button]')
+
+novaTarefa.addEventListener('click', criarTarefa)
